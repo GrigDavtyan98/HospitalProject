69aa2a91e9ad35c56125b1fd0d77ecd30c403c09
Admin <Admin@DESKTOP-550H722>
2023-09-19 10:18:52 +0400

    Добавьте файлы проекта.

---
+++ Hospital.DAL/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.DAL.Enitites
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Discription { get; set; }
+        public int Salary { get; set; }
+        public string Profession { get; set; }
+    }
+}
---
+++ Hospital.DAL/Reception.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.DAL.Enitites
+{
+    public class Reception
+    {
+        public int Id { get; set; }
+        
+    }
+}
